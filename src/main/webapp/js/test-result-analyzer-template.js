4b88a2d2f212422f2dc2610f7e616657cdb6043c
Matthias Fuchs <matthias.s.fuchs@gmail.com>
2017-01-02 16:59:13 +0100

    Every build-status has a title representing the build.
    This is useful when there are many tests since then the column title
    might not be visible anymore (scrolled down too much).
    Now a mouse over over the build status will e.g. show "Build 57".

--- src/main/webapp/js/test-result-analyzer-template.js
+++ src/main/webapp/js/test-result-analyzer-template.js
@@ -22,7 +22,8 @@ var tableContent = '<div class="table-row" name = "{{addName text}}" ' +
     '\n' + '<div class="table-cell" title="Builds (Tests)">{{percentPassed buildResults}}</div> ' +
     '\n' + '<div class="table-cell" title="Number of transitions from passed to failed and failed to passed.">{{numberTransitions buildResults}}</div> ' +
     '{{#each this.buildResults}}' +
-    '\n' + '         <div class="table-cell build-result {{applystatus status}}" data-result=\'{{JSON2string this}}\'><a href="{{url}}">{{applyvalue status totalTimeTaken}}</a></div>' +
+    '\n' + '         <div class="table-cell build-result {{applystatus status}}" data-result=\'{{JSON2string this}}\' ' +
+                          'title="Build {{buildNumber}}"><a href="{{url}}">{{applyvalue status totalTimeTaken}}</a></div>' +
     '{{/each}}' +
     '\n' + '</div>' +
     '{{#each children}}\n' +
