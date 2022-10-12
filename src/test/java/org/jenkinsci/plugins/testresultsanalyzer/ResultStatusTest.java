6cfade493c97a1ba108102271d890c33a6b40412
dipanshu <medipansh@gmail.com>
2022-10-12 23:02:45 +0530

    removed public access modifier
    Co-authored-by: Ullrich Hafner <ullrich.hafner@gmail.com>

--- src/test/java/org/jenkinsci/plugins/testresultsanalyzer/ResultStatusTest.java
+++ src/test/java/org/jenkinsci/plugins/testresultsanalyzer/ResultStatusTest.java
@@ -4,7 +4,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.junit.jupiter.api.Test;
 
-public class ResultStatusTest {
+class ResultStatusTest {
     @Test
     void testGetValue() {
         assertEquals("PASSED", ResultStatus.PASSED.getValue());
